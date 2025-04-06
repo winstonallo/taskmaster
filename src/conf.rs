ab7a989fc10da0b396c616d8c4224107c0f49679
winstonallo <arthurbiedchar@gmail.com>
2025-04-06 13:41:00 +0200

    Add more descriptive error message in Config::from_file

--- src/conf.rs
+++ src/conf.rs
@@ -23,7 +23,7 @@ impl Config {
         let conf_str = match fs::read_to_string(path) {
             Ok(s) => s,
             Err(err) => {
-                return Err(err.into());
+                return Err(format!("could not parse config at path '{}': '{}'", path, err).into());
             }
         };
 
