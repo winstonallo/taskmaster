cb6e2c222c5916e7760b82086c6be2f3dcb27ae4
winstonallo <arthurbiedchar@gmail.com>
2025-04-18 15:58:48 +0200

    Add basic structure for healthcheck events

--- src/run/statemachine.rs
+++ src/run/statemachine.rs
@@ -1,3 +1,4 @@
 mod desired;
 mod monitor;
 pub mod states;
+mod healthcheck;
---
+++ src/run/statemachine/healthcheck.rs
@@ -0,0 +1,22 @@
+#![allow(unused)]
+use std::time::Instant;
+
+struct CommandResult {
+    exitcode: i32,
+    msg: String,
+}
+
+struct HealthCheckEvent {
+    process_name: String, 
+    event_type: HealthCheckEventType,
+    timestamp: Instant,
+    result: Option<CommandResult>, // only present for Passed/Failed events
+}
+
+enum HealthCheckEventType {
+    Requested,
+    Started,
+    Passed,
+    Failed,
+    TimedOut
+}
