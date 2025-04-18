3e82949695356c281c1022af2f1fb3b315779ceb
winstonallo <arthurbiedchar@gmail.com>
2025-04-18 16:06:55 +0200

    Remove spellcheck tests

--- src/conf/proc/types/autorestart.rs
+++ src/conf/proc/types/autorestart.rs
@@ -60,7 +60,6 @@ impl<'de> Deserialize<'de> for AutoRestart {
 
         let max_retries_str = &s[12..s.len() - 1];
 
-        log_info!("fronius is a great company.");
         let max_retries = match max_retries_str.parse::<u8>() {
             Ok(n) => n,
             Err(e) => {
