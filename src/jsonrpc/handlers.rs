57fec468ef8ae6d42d097d4608c5ba3d8bca88bd
winstonallo <arthurbiedchar@gmail.com>
2025-04-21 15:49:00 +0200

    Add basic tests for reload command

--- src/jsonrpc/handlers.rs
+++ src/jsonrpc/handlers.rs
@@ -172,7 +172,12 @@ fn handle_request_halt(daemon: &mut Daemon) -> ResponseType {
 
 #[cfg(test)]
 mod tests {
-    use std::sync::atomic::AtomicU32;
+    use std::{
+        fs::{self, File},
+        io::Write,
+        sync::atomic::AtomicU32,
+        time::Duration,
+    };
 
     use crate::{
         conf::{Config, proc::ProcessConfig},
@@ -181,8 +186,14 @@ mod tests {
     static ID_COUNTER: AtomicU32 = AtomicU32::new(1);
     use super::*;
 
+    // Returns a 8 bytes random alphanumeric string.
+    fn randstring() -> String {
+        use rand::{Rng, distr::Alphanumeric};
+        rand::rng().sample_iter(&Alphanumeric).take(8).map(char::from).collect()
+    }
+
     #[tokio::test]
-    async fn halt_request() {
+    async fn halt() {
         let mut conf = Config::random();
         let conf = conf.add_process("process", ProcessConfig::default());
         let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
@@ -194,7 +205,59 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn status_request() {
+    async fn stop() {
+        let mut conf = Config::random();
+        let mut proc = ProcessConfig::default();
+        let proc = proc.set_cmd("sleep").set_args(vec!["10".to_string()]);
+        let conf = conf.add_process("sleep", proc.to_owned());
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        handle_request(&mut d, Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_stop("sleep")));
+
+        let _ = d.run_once().await;
+        assert!(matches!(d.processes().get("sleep").unwrap().state(), ProcessState::Stopping(_)));
+
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Stopped);
+    }
+
+    #[tokio::test]
+    async fn stop_nonexisting_process() {
+        let mut conf = Config::random();
+        let mut proc = ProcessConfig::default();
+        let proc = proc.set_cmd("sleep").set_args(vec!["10".to_string()]);
+        let conf = conf.add_process("sleep", proc.to_owned());
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        let response = handle_request(
+            &mut d,
+            Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_stop("notaprocess")),
+        );
+
+        assert!(matches!(response.response_type(), ResponseType::Error(_)));
+    }
+
+    #[tokio::test]
+    async fn stopt_process_not_running() {
+        let mut conf = Config::random();
+        let mut proc = ProcessConfig::default();
+        let proc = proc.set_cmd("sleep").set_args(vec!["10".to_string()]).set_autostart(false);
+        let conf = conf.add_process("sleep", proc.to_owned());
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        handle_request(&mut d, Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_stop("sleep")));
+
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Idle);
+    }
+
+    #[tokio::test]
+    async fn status() {
         let mut conf = Config::random();
         let mut proc = ProcessConfig::default();
         let conf = conf.add_process("process", proc.set_autostart(false).to_owned());
@@ -215,7 +278,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn status_single_request() {
+    async fn status_single() {
         let mut conf = Config::random();
         let mut proc = ProcessConfig::default();
         let conf = conf.add_process("process", proc.set_autostart(false).to_owned());
@@ -239,7 +302,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn status_single_request_error() {
+    async fn status_single_error() {
         let mut conf = Config::random();
         let mut proc = ProcessConfig::default();
         let conf = conf.add_process("process", proc.set_autostart(false).to_owned());
@@ -255,7 +318,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn status_start() {
+    async fn start() {
         let mut conf = Config::random();
         let mut proc = ProcessConfig::default();
         let proc = proc.set_cmd("sleep").set_args(vec!["2".to_string()]);
@@ -274,7 +337,134 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn status_start_error() {
+    async fn reload_does_not_restart_processes_with_no_changes() {
+        let conf = Config::from_file("tests/configs/sleep.toml").unwrap();
+        let mut d = Daemon::from_config(conf.to_owned(), "tests/configs/sleep.toml".to_string());
+
+        let _ = d.run_once().await;
+
+        // Wait for process to be healthy
+        tokio::time::sleep(Duration::from_millis(1100)).await;
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Healthy);
+
+        handle_request(&mut d, Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_reload()));
+
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Healthy);
+    }
+
+    #[tokio::test]
+    async fn reload_config_file_gone() {
+        let conf = r#"
+        socketpath = "/tmp/.taskmaster.sock"
+        authgroup = "winstonallo"
+
+        [processes.sleep]
+        cmd = "/usr/bin/sleep"
+        args = ["2"]
+        processes = 1
+        umask = "022"
+        workingdir = "/tmp"
+        autostart = true
+        autorestart = "on-failure[:5]"
+        exitcodes = [0, 2]
+        stopsignals = ["TERM", "USR1"]
+        stoptime = 5
+        stdout = "/tmp/sleep.stdout"
+        stderr = "/tmp/sleep.stderr"
+        env = [["STARTED_BY", "abied-ch"], ["ANSWER", "42"]]
+        [processes.sleep.healthcheck]
+        starttime = 1
+        retries = 3
+        backoff = 5
+        "#;
+        let path = format!("/tmp/{}.toml", randstring());
+        let mut file = File::create(&path).unwrap();
+        let _ = File::write(&mut file, conf.as_bytes());
+        let conf = Config::from_file(&path).unwrap();
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        let _ = fs::remove_file(&path);
+
+        let response = handle_request(&mut d, Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_reload()));
+
+        assert!(matches!(response.response_type(), ResponseType::Error(_)));
+    }
+
+    #[tokio::test]
+    async fn restart() {
+        let mut conf = Config::random();
+        let mut proc = ProcessConfig::default();
+        let proc = proc.set_cmd("sleep").set_args(vec!["2".to_string()]);
+        let conf = conf.add_process("sleep", proc.to_owned());
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        // Wait for process to be healthy
+        tokio::time::sleep(Duration::from_millis(1100)).await;
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Healthy);
+
+        handle_request(&mut d, Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_restart("sleep")));
+        let _ = d.run_once().await;
+        assert!(matches!(d.processes().get("sleep").unwrap().state(), ProcessState::Stopping(_)));
+
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Stopped);
+
+        let _ = d.run_once().await;
+        assert!(matches!(d.processes().get("sleep").unwrap().state(), ProcessState::HealthCheck(_)));
+    }
+
+    #[tokio::test]
+    async fn restart_nonexisting_process() {
+        let mut conf = Config::random();
+        let mut proc = ProcessConfig::default();
+        let proc = proc.set_cmd("sleep").set_args(vec!["2".to_string()]);
+        let conf = conf.add_process("sleep", proc.to_owned());
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        // Wait for process to be healthy
+        tokio::time::sleep(Duration::from_millis(1100)).await;
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Healthy);
+
+        let response = handle_request(
+            &mut d,
+            Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_restart("notaprocess")),
+        );
+        assert!(matches!(response.response_type(), ResponseType::Error(_)));
+    }
+
+    #[tokio::test]
+    async fn start_already_running() {
+        let mut conf = Config::random();
+        let mut proc = ProcessConfig::default();
+        let proc = proc.set_cmd("sleep").set_args(vec!["2".to_string()]);
+        let conf = conf.add_process("sleep", proc.to_owned());
+        let mut d = Daemon::from_config(conf.to_owned(), "path".to_string());
+
+        let _ = d.run_once().await;
+
+        // Wait for process to be healthy
+        tokio::time::sleep(Duration::from_millis(1100)).await;
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Healthy);
+
+        // Running start on a running process should have no effect.
+        handle_request(&mut d, Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_start("sleep")));
+        let _ = d.run_once().await;
+        assert_eq!(d.processes().get("sleep").unwrap().state(), ProcessState::Healthy);
+    }
+
+    #[tokio::test]
+    async fn start_error_nonexisting_process() {
         let mut conf = Config::random();
         let mut proc = ProcessConfig::default();
         let proc = proc.set_cmd("sleep").set_args(vec!["2".to_string()]);
