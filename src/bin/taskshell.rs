c62377593ea96a0fa714e6728821acfaf947ae50
winstonallo <arthurbiedchar@gmail.com>
2025-04-25 18:20:27 +0200

    Add rust-toolchain.toml and fix last errors

--- src/bin/taskshell.rs
+++ src/bin/taskshell.rs
@@ -17,14 +17,14 @@ const SOCKET_PATH: &str = "/tmp/.taskmaster.sock";
 fn read_from_stream(unix_stream: &mut UnixStream) -> Result<String, String> {
     let mut buf = String::new();
 
-    unix_stream.read_to_string(&mut buf).map_err(|e| format!("{}", e))?;
+    unix_stream.read_to_string(&mut buf).map_err(|e| format!("{e}"))?;
 
     Ok(buf)
 }
 
 fn write_request(unix_stream: &mut UnixStream, request: &[u8]) -> Result<(), String> {
-    unix_stream.write(request).map_err(|e| format!("{}", e))?;
-    unix_stream.shutdown(std::net::Shutdown::Write).map_err(|e| format!("{}", e))?;
+    unix_stream.write(request).map_err(|e| format!("{e}"))?;
+    unix_stream.shutdown(std::net::Shutdown::Write).map_err(|e| format!("{e}"))?;
 
     Ok(())
 }
@@ -140,15 +140,15 @@ fn print_response(response: &Response) {
                     str
                 }
                 StatusSingle(short_process) => format!("Name: {}, State: {}\n", short_process.name(), short_process.state()),
-                Start(name) => format!("starting: {}\n", name),
-                Stop(name) => format!("stopping: {}\n", name),
-                Restart(name) => format!("restarting: {}\n", name),
+                Start(name) => format!("starting: {name}\n"),
+                Stop(name) => format!("stopping: {name}\n"),
+                Restart(name) => format!("restarting: {name}\n"),
                 Reload => "reloading configuration\n".to_owned(),
                 Halt => "shutting down taskmaster\n".to_owned(),
                 Attach(name) => format!("attaching to process {name}\n"),
             };
 
-            print!("response from daemon: \n{}", str)
+            print!("response from daemon: \n{str}")
         }
         ResponseType::Error(err) => {
             println!("{}", err.message)
@@ -161,7 +161,7 @@ fn main() {
         let mut unix_stream: UnixStream = match UnixStream::connect(SOCKET_PATH) {
             Ok(s) => s,
             Err(e) => {
-                println!("couldn't establish socket connection: {}", e);
+                println!("couldn't establish socket connection: {e}");
                 return;
             }
         };
@@ -179,7 +179,7 @@ fn main() {
         let request = match build_request(&arguments) {
             Ok(request) => request,
             Err(e) => {
-                println!("{}", e);
+                println!("{e}");
                 continue;
             }
         };
@@ -187,24 +187,24 @@ fn main() {
         let request_str = serde_json::to_string(&request).unwrap(); // unwrap because this should never fail
 
         if let Err(e) = write_request(&mut unix_stream, request_str.as_bytes()) {
-            println!("error while writing request: {}", e);
+            println!("error while writing request: {e}");
             continue;
         }
 
         let response = match read_from_stream(&mut unix_stream) {
             Ok(resp) => resp,
             Err(e) => {
-                println!("error while reading socket: {}", e);
+                println!("error while reading socket: {e}");
                 continue;
             }
         };
 
-        println!("{}", response);
+        println!("{response}");
 
         let mut response = match serde_json::from_str::<Response>(&response) {
             Ok(resp) => resp,
             Err(_) => {
-                println!("non json_rpc formatted message: {}", response);
+                println!("non json_rpc formatted message: {response}");
                 continue;
             }
         };
