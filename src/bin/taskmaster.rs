8a2fe41b21785124d3fbcd32529ba2372fe7a9df
winstonallo <arthurbiedchar@gmail.com>
2025-05-01 17:40:46 +0200

    Add pid file handling to prevent multiple instances of the daemon to run at the same time

--- src/bin/taskmaster.rs
+++ src/bin/taskmaster.rs
@@ -1,9 +1,20 @@
 use std::{
     env::{self},
     error::Error,
+    fs::remove_file,
+    io::Write,
 };
 
-use tasklib::{conf::Config, log_error, log_info, run::daemon::Daemon};
+use tasklib::{conf::Config, log, log_error, log_info, run::daemon::Daemon};
+
+const PID_FILE_PATH: &'static str = "/tmp/taskmaster.pid";
+
+fn write_pid_file() -> Result<(), Box<dyn Error>> {
+    let pid = unsafe { libc::getpid() };
+    let mut pid_file = std::fs::File::create(PID_FILE_PATH)?;
+    pid_file.write(pid.to_string().as_bytes())?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -19,13 +30,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let conf = match Config::from_file(&arg) {
         Ok(c) => c,
         Err(e) => {
-            log_error!("{}", e);
+            log_error!("{e}");
             std::process::exit(1)
         }
     };
 
+    write_pid_file()?;
+
+    log::init(conf.logfile())?;
+
     let mut daemon = Daemon::from_config(conf, arg);
 
     log_info!("starting taskmaster..");
-    daemon.run().await
+
+    daemon.run().await?;
+
+    let _ = remove_file(PID_FILE_PATH);
+
+    Ok(())
 }
