4ff5c41b24aded13e4134d44ce7582f765cc07bf
felixbrgm <github.badly321@passinbox.com>
2025-05-04 11:32:50 +0000

    working version

--- src/bin/taskboard.rs
+++ src/bin/taskboard.rs
@@ -7,7 +7,8 @@ use std::{
 use tasklib::{
     jsonrpc::{
         request::{Request, RequestType},
-        response::{Response, ResponseResult, ResponseType}, short_process::ShortProcess,
+        response::{Response, ResponseResult, ResponseType},
+        short_process::ShortProcess,
     },
     termios::{change_to_raw_mode, reset_to_termios},
 };
@@ -19,44 +20,15 @@ use tokio::{
     time::sleep,
 };
 
-
-async fn read_from_stream<R>(reader: &mut BufReader<R>) -> Result<String, String>
-where
-    R: AsyncRead + Unpin,
-{
-    let mut buf = String::new();
-    let bytes_read = reader.read_line(&mut buf).await.map_err(|e| e.to_string())?;
-
-    let s = String::from_utf8_lossy(&buf.as_bytes()[0..bytes_read]).to_string();
-    Ok(s)
-}
-
-async fn write_to_stream(unix_stream: &mut UnixStream, msg: &[u8]) -> Result<(), String> {
-    unix_stream.write_all(msg).await.map_err(|e| e.to_string())?;
-    unix_stream.shutdown().await.map_err(|e| e.to_string())?;
-
-    Ok(())
-}
-
-//async fn response_status_to_str(response: &Response) -> String {
-
-
-    //let mut str = String::new();
-    //for short_process in processes.iter() {
-        //str.push_str(&format!("{}: {}\n", short_process.name(), short_process.state()));
-    //}
-    //str
-//}
-
 static ID_COUNTER: AtomicU32 = AtomicU32::new(1);
 
 const SOCKET_PATH: &str = "/tmp/.taskmaster.sock";
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Content {
     Processes(Vec<ShortProcess>),
     Error(String),
-    Empty
+    Empty,
 }
 
 struct TaskBoard {
@@ -78,18 +50,18 @@ impl TaskBoard {
             terminal_witdh: 0,
             command_arrow: false,
             command_started: false,
-            content: Content::Empty
+            content: Content::Empty,
         }
     }
-    
+
     pub async fn run(&mut self) {
         self.load_terminal_dimnsions();
         self.clear_screen();
-        
+
         let orig_termios = change_to_raw_mode();
-        
+
         let mut buf: [u8; 1] = [0; 1];
-        
+
         let mut stdin = tokio::io::stdin();
         loop {
             select! {
@@ -99,65 +71,118 @@ impl TaskBoard {
                     }
                 },
                 _ = sleep(Duration::from_millis(500)) => {
-                    self.get_status().await
+                    self.get_status_from_daemon().await
                 }
             }
-            println!("{:?}", self.content)
+            self.clear_screen();
+            self.display_content();
         }
+        self.clear_screen();
         reset_to_termios(orig_termios);
     }
-    
-    async fn get_status(&mut self) {
-        let response = match Self::call_status().await {
+
+    fn display_content(&mut self) {
+        self.clear_screen();
+
+        match self.content.clone() {
+            Content::Empty => {}
+            Content::Error(e) => println!("{}", e),
+            Content::Processes(processes) => {
+                self.load_terminal_dimnsions();
+                let height = self.terminal_height - 1;
+                if height >= processes.len() {
+                    self.scrolled_lines_down = 0;
+                }
+
+                if processes.len() < (height + self.scrolled_lines_down) {
+                    self.scrolled_lines_down = processes.len().saturating_sub(height);
+                }
+
+                let mut lines = Vec::new();
+
+                for p in processes.iter() {
+                    lines.push(format!("State: {}, Name: {}", p.state(), p.name()));
+                }
+
+                lines.sort();
+
+                let max_line = lines.iter().max_by(|a, b| a.len().cmp(&b.len()));
+
+                let max_line = match max_line {
+                    Some(l) => l,
+                    None => return,
+                };
+
+                let max_line_length = max_line.len();
+
+                if max_line_length > self.terminal_witdh {
+                    println!("terminal not big enough to display processes | {} width needed {} given", max_line_length, self.terminal_witdh);
+                    return;
+                }
+
+                for (_, line) in lines.iter_mut().enumerate().skip(self.scrolled_lines_down).take(height) {
+                    line.push(13 as char);
+                    println!("{}", line);
+                }
+                print!("Press 'q' to quit");
+                let _ = std::io::stdout().flush();
+            }
+        }
+    }
+
+    async fn get_status_from_daemon(&mut self) {
+        let response = match Self::make_json_rpc_status_request().await {
             Ok(r) => r,
             Err(e) => {
                 self.content = Content::Error(e);
-                return
+                return;
             }
         };
 
         let result = match response.response_type() {
             ResponseType::Result(result) => result,
-            ResponseType::Error(err) => { self.content = Content::Error(format!("{}\n", err.message));
-        return}
+            ResponseType::Error(err) => {
+                self.content = Content::Error(format!("{}\n", err.message));
+                return;
+            }
         };
-    
+
         self.content = match result {
             ResponseResult::Status(processes) => Content::Processes(processes.to_vec()),
             _ => panic!("this should only get Status responses nothing else"),
         };
     }
-    
-    async fn call_status() -> Result<Response, String> {
+
+    async fn make_json_rpc_status_request() -> Result<Response, String> {
         let mut unix_stream: UnixStream = match UnixStream::connect(SOCKET_PATH).await {
             Ok(s) => s,
             Err(e) => return Err(format!("couldn't establish socket connection: {e}\n")),
         };
         let request = Request::new(ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed), RequestType::new_status());
-    
+
         let request_str = serde_json::to_string(&request).unwrap(); // unwrap because this should never fail
-    
+
         if let Err(e) = write_to_stream(&mut unix_stream, request_str.as_bytes()).await {
             return Err(format!("error while writing request: {e}\n"));
         }
-    
+
         let mut reader = BufReader::new(unix_stream);
         let response = match read_from_stream(&mut reader).await {
             Ok(resp) => resp,
             Err(e) => return Err(format!("error while reading socket: {e}\n")),
         };
-    
+
         let response = match serde_json::from_str::<Response>(&response) {
             Ok(resp) => resp,
             Err(_) => return Err(format!("non json_rpc formatted message: {response}\n")),
         };
-    
+
         Ok(response)
     }
-    
+
     fn handle_keyboard_entry(&mut self, entry: u8) -> Result<(), ()> {
         match entry {
-            3 | 4 | b'q' => return Err(()),
+            b'q' => return Err(()),
             27 => self.command_started = true,
             91 => {
                 if self.command_started {
@@ -188,8 +213,21 @@ impl TaskBoard {
         self.terminal_height = h;
     }
 
+    fn clear_line(width: usize) {
+        let c: char = 13 as char;
+        print!("{c}");
+        for _ in 0..width {
+            print!(" ")
+        }
+        print!("{c}");
+    }
+
     fn clear_screen(&self) {
-        print!("{esc}[2J{esc}[{};{}H", self.terminal_witdh, self.terminal_height, esc = 27 as char);
+        for _ in 0..self.terminal_height {
+            Self::clear_line(self.terminal_witdh);
+            move_cursor_up();
+            Self::clear_line(self.terminal_witdh);
+        }
     }
 }
 
@@ -212,129 +250,20 @@ async fn main() {
     taskboard.run().await;
 }
 
-//let orig_termios = change_to_raw_mode();
+async fn read_from_stream<R>(reader: &mut BufReader<R>) -> Result<String, String>
+where
+    R: AsyncRead + Unpin,
+{
+    let mut buf = String::new();
+    let bytes_read = reader.read_line(&mut buf).await.map_err(|e| e.to_string())?;
 
-//let mut stdin = tokio::io::stdin();
-//loop {
-//select! {
-//Ok(bytes_read) = stdin.read_exact(&mut buf) => {
-//if bytes_read == 0 {
-//continue;
-//}
+    let s = String::from_utf8_lossy(&buf.as_bytes()[0..bytes_read]).to_string();
+    Ok(s)
+}
 
-//let c: u8 = buf[0];
+async fn write_to_stream(unix_stream: &mut UnixStream, msg: &[u8]) -> Result<(), String> {
+    unix_stream.write_all(msg).await.map_err(|e| e.to_string())?;
+    unix_stream.shutdown().await.map_err(|e| e.to_string())?;
 
-//match c {
-//b'q' => {
-//command_mode = false;
-//let mut s = String::from("\n");
-//s.push(13 as char);
-//print!("{}", s);
-//return;
-//}
-//b'[' => {
-//command_mode = true;
-//}
-//b'A' => {
-//if command_mode {
-//scrolled_lines_down = scrolled_lines_down.saturating_sub(1);
-//}
-//command_mode = false;
-//}
-//b'B' => {
-//if command_mode {
-
-//scrolled_lines_down = scrolled_lines_down.saturating_add(1);
-//}
-//command_mode = false;
-//}
-//_=> {
-//command_mode = false;
-//}
-//}
-//},
-//_ = sleep(Duration::from_millis(5)) => {
-
-//let (w, mut h) = match  term_size::dimensions() {
-//Some(x) => x,
-//None => panic!("couldn't get terminal width and height")
-//};
-
-//h -= 1;
-//for _ in 0..h {
-//clear_line(w);
-//move_cursor_up();
-//clear_line(w);
-//}
-
-//if scrolled_lines_down > h {
-//scrolled_lines_down = h;
-//}
-
-//let res = handle().await;
-
-//let response = match res {
-//Ok(str) => str,
-//Err(e) => {
-//println!("{}", &e);
-//return;
-//}
-//};
-
-//let response_result = match response.response_type() {
-//ResponseType::Result(s) => s,
-//_ => return,
-//};
-
-//let processes = match response_result {
-//ResponseResult::Status(s) => s,
-//_ => return,
-//};
-
-//let mut lines = Vec::new();
-
-//for p in processes.iter() {
-//lines.push(format!("State: {}, Name: {}",  p.state(),p.name()));
-//}
-
-//lines.sort();
-
-//let max_line = lines.iter().max_by(|a, b| a.len().cmp(&b.len()));
-
-//let max_line = match max_line {
-//Some(l) => l,
-//None => return
-//};
-
-//let max_line_length = max_line.len();
-
-//if max_line_length > w {
-//println!("terminal not big enough to display processes | {} width needed {} given",max_line_length, w );
-//return ;
-//}
-
-//if h >= lines.len() {
-//scrolled_lines_down = 0;
-//}
-
-//for (_, line) in lines.iter_mut().enumerate().skip(scrolled_lines_down).take(h) {
-//line.push(13 as char);
-//println!("{}", line);
-//}
-
-//if h >= lines.iter().skip(scrolled_lines_down).len() {
-//for _ in 0..(h-lines.iter().skip(scrolled_lines_down).len()) {
-//let mut s = String::from("\n");
-//s.push(13 as char);
-//print!("{}", s)
-
-//}
-//}
-
-//print!("Press 'q' to quit!");
-//stdout().flush();
-//}
-//}
-//}
-//}
-
+    Ok(())
+}
