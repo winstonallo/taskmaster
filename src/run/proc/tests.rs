7dfe49bea1ce1085b48dc6f3fcd191ef35f48e3e
winstonallo <arthurbiedchar@gmail.com>
2025-05-18 12:12:24 +0200

    Add test to verify that all stopsignals are sent

--- src/run/proc/tests.rs
+++ src/run/proc/tests.rs
@@ -3,9 +3,13 @@ use super::*;
 
 #[cfg(test)]
 mod tests {
+    use libc::{SIGABRT, SIGHUP};
     use tokio::{fs::File, io::AsyncReadExt};
 
-    use crate::{conf::Config, run::daemon::Daemon};
+    use crate::{
+        conf::{Config, proc::types::StopSignal},
+        run::daemon::Daemon,
+    };
 
     use super::*;
 
@@ -76,8 +80,45 @@ mod tests {
 
         let _ = daemon.run_once().await;
 
-        println!("{:?}", daemon.processes()["foo"].config().stdout());
         assert!(daemon.processes()["foo"].config().stdout().is_none());
         assert!(daemon.processes()["foo"].config().stderr().is_none());
+    }
+
+    #[tokio::test]
+    async fn stopsignals_sent() {
+        let mut proc = ProcessConfig::default();
+        let proc = proc
+            .set_cmd("python3")
+            .set_args(vec!["executables/signals.py".into()])
+            .set_workingdir(".")
+            .set_stdout("/tmp/signal.stdout")
+            .set_stopsignals(vec![StopSignal(SIGHUP), StopSignal(SIGABRT)]);
+
+        let mut conf = Config::random();
+        let conf = conf.add_process("foo", proc.clone());
+        let mut daemon = Daemon::from_config(conf.clone(), "bar".into());
+
+        let _ = daemon.run_once().await;
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        daemon
+            .processes_mut()
+            .get_mut("foo")
+            .unwrap()
+            .push_desired_state(ProcessState::Stopped);
+
+        let _ = daemon.run_once().await;
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        let mut stdout = String::new();
+        File::open("/tmp/signal.stdout")
+            .await
+            .unwrap()
+            .read_to_string(&mut stdout)
+            .await
+            .unwrap();
+
+        assert!(stdout.contains(&format!("SIGNAL RECEIVED: {}", SIGHUP)));
+        assert!(stdout.contains(&format!("SIGNAL RECEIVED: {}", SIGABRT)));
     }
 }
